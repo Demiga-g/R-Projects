03e1b7d6ad2e5ad000f6940292410d8d6f4d5349
Demiga-g <midegageorge2@gmail.com>
2023-02-18 22:40:59 +0300

    Performed analysis and looked for various ways to visualize the data

---
+++ Women in Autonomous Decison Making/rough_work.R
@@ -0,0 +1,21 @@
+
+# Turning Data into surveydata object
+
+library(surveydata, quietly=TRUE)
+library(likert)
+
+
+women_vs_men <- females_wider[, c(1:15)] 
+questions <- colnames(women_vs_men)
+colnames(women_vs_men) <- c("country", paste0("Q_", 1:14))
+colnames(women_vs_men)
+
+varlabels(women_vs_men) <- questions
+women_vs_men <- as.surveydata(women_vs_men, renameVarlabel=TRUE)
+
+p1 <- survey_plot_yes_no(women_vs_men, "Q_2")
+p1
+women_vs_men
+membersurvey
+as.surveydata(females_wider)
+survey_plot_yes_no(as.surveydata(females_wider), `A woman can apply for a passport in the same way as a man (1=yes; 0=no)`)
