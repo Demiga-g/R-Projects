19d54011b949e9d4f0f6c26e73da272e62ba3d69
Demiga-g <midegageorge2@gmail.com>
2023-02-25 20:58:39 +0300

    Collected data about equality and data about women, business, and the law

--- Women in Autonomous Decison Making/rough_work.R
+++ Women in Autonomous Decison Making/rough_work.R
@@ -1,4 +1,8 @@
 
+        
+  # ------- First Exploration --------
+    
+
 # Turning Data into surveydata object
 
 library(surveydata, quietly=TRUE)
@@ -23,6 +27,10 @@ survey_plot_yes_no(as.surveydata(females_wider), `A woman can apply for a passpo
 
 map(database="world", region="Democratic Republic of the Congo")
 map(database="world", region=c("Republic of Congo"))
+
+  
+
+          # --------- Second Exploration -------
 
 
 females <- read.csv("women_autonomous_decisions.csv", na.strings="..")
@@ -103,3 +111,132 @@ a <- continents %>%
   left_join(coords, by=c("Country"="country"))
 
 #write.csv(a, "africa_clean_coords.csv", row.names=FALSE)
+
+
+
+
+      # ----------- Third Exploration -----------
+
+library(tidyverse, quietly=TRUE)
+library(scales)
+
+
+# Cape verde, Congos, Egypt, Gambia, Cote'dvore, Eswatini
+
+
+raw_data <- read.csv("yearly_progression.csv")[, -c(4)]
+clean_1 <- raw_data %>%
+  pivot_longer(cols=contains("..YR", ignore.case=FALSE), names_to="year", 
+               values_to="values") %>%
+  rename(region=Country.Name, `region code`=Country.Code, 
+         variables=Series.Name)%>%
+  pivot_wider(., names_from=variables, values_from=values) %>%
+  mutate(year=str_sub(year, 2, 5))%>%
+  mutate_at(c('year'), as.numeric)
+
+equality1 <- clean_1[, 1:17]
+health1 <- clean_1[, c(1:3, 18, 19, 22, 21, 20)]
+visits1 <- clean_1[, c(1:3, 23:26, 32)]
+purchases1 <- clean_1[, c(1:3, 27:31)]
+participation1 <- clean_1[, c(1:3, 35, 36, 34, 37, 39, 40, 38)]
+
+
+
+## Participation
+
+"Percentage of women participating in decisions regarding "
+recode.participation <- c("region", "region code", "year", 
+                          "what food to cook daily", 
+                          "daily purchases",
+                          "visits to family, relative, and friends",
+                          "major household purchases", 
+                          "their own health care", 
+                          "all the three decisions", 
+                          "none of the three decisions")
+
+colnames(participation1) <- recode.participation
+
+participation <- participation1 %>%
+  pivot_longer(cols=recode.participation[4:10], names_to="decisions", 
+               values_to="percentage")
+
+participation %>%
+  filter(year==1990, decisions=="what food to cook daily")
+
+
+
+"Decision maker about a woman's own health care: "
+recode.health <- c("region", "region code", "year", "mainly husband", 
+                   "mainly wife", "wife and husband jointly", "someone else", 
+                   "other")
+
+colnames(health1) <- recode.health
+
+health <- health1 %>%
+  pivot_longer(cols=recode.health[4:8], names_to="decisions", 
+               values_to="percentage")
+
+all_decisions <- health %>%
+  filter(year==2020, decisions=="wife and husband jointly")
+
+
+# Data to Plot
+plot_decisions <- africa_map %>%
+  left_join(all_decisions, by=c("region"))
+
+# Labels to use
+
+country_labels <- africa_countries %>%
+  left_join(plot_decisions, by=c("Country"="region")) %>%
+  distinct(Country, .keep_all=TRUE) %>%
+  select(-c("long", "lat")) %>%
+  rename(lat=latitude, long=longitude)
+
+# Map Plot
+
+ggplot(plot_decisions, aes(x=long, y=lat, group=group))+
+  coord_fixed(1)+
+  geom_polygon(aes(fill=percentage), color="#ffefdb")+
+  #scale_fill_manual(values=c("#0b84a5", "#f17853", "#ffb600"), 
+  #                  labels=(c("No", "Yes", "No Data")), 
+  #                  name=unique(all_response$opinion)) +
+  labs(title=paste0("A woman can ", {unique(all_decisions$decisions)}, 
+                    " the same way as a man, ", {unique(all_decisions$year)}), 
+       caption=paste0("Data Source: World Bank\n", "Last Update 2023/01/31\n", 
+                      "Author: Midega George")) +
+  ggrepel::geom_label_repel(aes(label= country_code), data= country_labels, 
+                            size=2.5, max.overlaps = 25, label.size=0, 
+                            arrow=arrow(length=unit(0.1, 'cm'))) +
+  theme(axis.text=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title=element_blank(),
+        rect=element_blank(),
+        axis.line = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        panel.background = element_rect(fill = "#b1d3bc"),
+        plot.title = element_text(hjust = 0.5),
+        legend.position="right", legend.title=element_text(size=10), 
+        legend.text=element_text(size=10))
+
+
+# Health
+
+females <- read.csv("women_autonomous_decisions.csv", na.strings="..")
+
+females_narrower <- females[, c(1, 3, 4, 7)]
+females_narrower
+
+colnames(females_narrower) <- c("name", "region", "code", "response")
+females_narrower$region[females_narrower$region == "Congo, Dem. Rep."] <- 
+  "Democratic Republic of the Congo"
+
+females_wider <- pivot_wider(females_narrower, names_from=name, 
+                             values_from=response)
+
+females_wider
+women_vs_men <- females_wider[, c(1:16)]
+colnames(women_vs_men) <- c("region", "code", paste0("Q_", 1:15))
+women_vs_men[, ] <- lapply(women_vs_men[,], factor)
+
+females_wider[, c(100, 99, 94, 95)]
