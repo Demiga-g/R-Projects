83d7068966425089938ca2d0547e1db50bdcb970
Demiga-g <midegageorge2@gmail.com>
2023-03-05 07:33:08 +0300

    Made changes to the apps outlook
    Created navigation tabs in the `Visualization` landing page to cater for
    the WBL and GE data.
    Removed the GE data from the sidebar and incorporated its analysis with
    that of WBL

--- Women in Autonomous Decison Making/ShinyApp/ui.R
+++ Women in Autonomous Decison Making/ShinyApp/ui.R
@@ -4,6 +4,7 @@ library(shinycssloaders)
 
 dashboardPage(
   dashboardHeader(
+    
     title = "Gender Statistics", 
     titleWidth=700,
     tags$li(class="dropdown", tags$a(href="https://github.com/Demiga-g/R-Projects/tree/main/Women%20in%20Autonomous%20Decison%20Making", icon("github"), "Source Code", target="_blank")),
@@ -13,10 +14,7 @@ dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       id = "sidebar",
-      menuItem(text="Women, Business, and Law", tabName="WBL", icon=icon("female")),
-      menuItem(text="Gender Equality", tabName="GE", icon=icon("balance-scale")),
-      conditionalPanel("input.sidebar == 'GE' && input.tab_2 == 'map_2'", selectInput("opinions", label="Opinion", choices=ge_ops)),
-      conditionalPanel("input.sidebar == 'GE' && input.tab_2 == 'map_2'", selectInput("ge_years", label="Year", choices=ge_yrs))
+      menuItem(text="Women, Business, and Law", tabName="WBL", icon=icon("female"))
     )
   ),
   
@@ -39,31 +37,54 @@ dashboardPage(
                    )
                    
           ),
-          tabPanel(title="Visualization", icon=icon("globe"), value="map_1",
-                   fluidRow(
-                     column(4, selectInput("indicators", label="Select an Indicator", choices=wbl_inds)),
-                     column(4, selectInput("wbl_years", label="Select a Year", choices=wbl_yrs)),
-                   ),
-                   fluidRow(column(8, withSpinner(plotOutput("wbl_map", height=520, width=820))),
-                            column(4, textInput("region_code", label="Region Code", placeholder="DZ"),
-                                   textOutput("region_name1"))
-                   )
-                   
+          tags$style(
+            HTML(".tabbable > .nav > li[class=active]    > a {background-color: #BFF7BB; color:black}")
           ),
+          tabPanel(title="Visualization", icon=icon("globe"), value="map_1",
+                   tabsetPanel(
+                     tabPanel(title="WBL",
+                       sidebarLayout(
+                         sidebarPanel(
+                           selectInput("indicators", label="Select an Indicator", choices=wbl_inds),
+                           selectInput("wbl_years", label="Select a Year", choices=wbl_yrs),
+                           textInput("region_code", label="Region Code", placeholder="DZ"),
+                           textOutput("region_name1")
+                         ),
+                         mainPanel(withSpinner(plotOutput("wbl_map", height=550, width=820)))
+                       )
+                     ),
+                     
+                     tabPanel(title="GE",
+                              sidebarLayout(
+                                sidebarPanel(
+                                  selectInput("opinions", label="Opinion", choices=ge_ops),
+                                  selectInput("ge_years", label="Year", choices=ge_yrs),
+                                  textInput("region_code", label="Region Code", placeholder="DZ"),
+                                  textOutput("region_name2")
+                                ),
+                                mainPanel(withSpinner(plotOutput("ge_map", height=550, width=820)))
+                              )
+                     )
+                   )
+          ),
+          
           tabPanel(
             title="Statistical Concept and Methodology", icon=icon("lightbulb"),
             fluidRow(
               br(),
-              column(6, infoBoxOutput("con_meth1", width=12)),
-              column(6, infoBoxOutput("con_meth2", width=12))
+              column(3),
+              column(9, infoBoxOutput("con_meth1", width=12)),
             ),
             fluidRow(
               column(2),
-              column(8, infoBoxOutput("con_meth3", width=12)),
+              column(10, infoBoxOutput("con_meth2", width=12)),
             ),
             fluidRow(
               column(1),
-              column(11, infoBoxOutput("con_meth4", width=12))
+              column(11, infoBoxOutput("con_meth3", width=12))
+            ),
+            fluidRow(
+              column(12, infoBoxOutput("con_meth4", width=12))
             )
             
           ),
@@ -72,9 +93,7 @@ dashboardPage(
                      column(12, p("The Women, Business and the Law methodology has limitations that should be considered when interpreting the data. All eight indicators are based on standardized assumptions to ensure comparability across economies. Comparability is one of the strengths of the data, but the assumptions can also be limitations as they may not capture all restrictions or represent all particularities in a country.",
                                   style="background-color:papayawhip;border-left:8px solid coral;border-top:1px solid black;border-bottom:1px solid black;border-right: 1px solid black"))
                    ),
-                   tags$style(
-                     HTML(".tabbable > .nav > li[class=active]    > a {background-color: #BFF7BB; color:black}")
-                   ),
+                   
                    tabsetPanel(
                      tabPanel(title="Assumptions Regarding Females",
                               fluidRow(
@@ -91,21 +110,7 @@ dashboardPage(
                                 column(12, infoBoxOutput("lim_exp3", width=12))
                               )
                      )
-                   ),
-          )
-        )
-      ),
-      
-      tabItem(
-        tabName="GE", 
-        tabBox(
-          id="tab_2", width=12,
-          tabPanel(title="About", icon=icon("info-circle"), h4("Placeholder tab1")),
-          tabPanel(title="Visualization", icon=icon("globe"), value="map_2",
-                   fluidRow(column(10, withSpinner(plotOutput("ge_map", height=520, width=1000))),
-                            column(2, textInput("region_code", label="Region Code", placeholder="DZ"),
-                                   textOutput("region_name2"))
-                  )
+                   )
           )
         )
       )
